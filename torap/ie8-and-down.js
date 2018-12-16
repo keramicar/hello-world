5a5e53864abb1642ff36f4ceecb242805884270d
keramicar <45180409+keramicar@users.noreply.github.com>
2018-12-16 23:45:12 +0100

    Add files via upload

---
+++ torap/ie8-and-down.js
@@ -0,0 +1,7 @@
+﻿document.createElement("header");
+document.createElement("main");
+document.createElement("footer");
+document.createElement("nav");
+document.createElement("section");
+document.createElement("aside");
+document.createElement("article");
---
+++ torap/index.js
@@ -0,0 +1,55 @@
+﻿
+
+let allpages = document.querySelectorAll('main');
+let allbutton = document.querySelectorAll('button');
+ current();
+ 
+ 
+ 
+
+ function current() {
+    removePage();
+    allpages[0].style.display = 'block';
+    buttons();
+    allbutton[0].classList.add('current');
+ 
+ }
+
+ function buttons() {
+  for ( var i = 0; i < allbutton.length; i++) {
+  
+  allbutton[i].classList.remove('current');
+  
+ }
+ }
+
+ function removePage() {
+   for (let i = 0; i < allpages.length; i++) {
+    allpages[i].style.display = 'none';
+ }
+ }
+
+ function changePage() {
+  if ((allpages[0].style.display = 'block') || (allpages[2].style.display ='block')) {
+    removePage();
+    allpages[1].style.display = 'block';
+    buttons();
+    allbutton[1].classList.add('current');
+
+   
+   
+    
+    
+ }
+ }
+
+ function changePage2() {
+  if ((allpages[0].style.display = 'block') || (allpages[1].style.display = 'block')) {
+    removePage();
+    allpages[2].style.display = 'block';
+     buttons();
+     allbutton[2].classList.add('current');
+    
+ }
+ }
+
